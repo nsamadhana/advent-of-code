dfa2909dbe57759871a5c6d14b7796b41a5e3e13
Nevan Samadhana <nsamadhana@gmail.com>
2019-12-23 14:30:53 -0800

    Day 4 part1

---
+++ 2019/december/day4/solution.py
@@ -0,0 +1,28 @@
+#Day 4 Secure Container 
+#Input range: 231832-767346
+'''
+Two adjacent digits are the same (like 22 in 122345).
+Going from left to right, the digits never decrease; 
+they only ever increase or stay the same (like 111123 or 135679)
+'''
+
+def isPair(a):
+    for i in range(1,len(a)-1):
+        if a[i]==a[i+1] or a[i]==a[i-1]: 
+            return True
+    return False 
+
+def isIncreasing(a):
+    for i in range(1, len(a)):
+        if a[i] < a[i-1]:
+            return False
+    return True 
+    
+lb = 231832
+rb = 767346
+count = 0 
+while lb < rb: 
+    if isPair(str(lb)) and isIncreasing(str(lb)):
+        count += 1 
+    lb += 1 
+print(count)
