7ebbe2fd791076ded4335511dde96632141fe689
Nevan Samadhana <nsamadhana@gmail.com>
2019-12-23 13:24:34 -0800

    Refactored day3 part1

--- 2019/december/day3/solution.py
+++ 2019/december/day3/solution.py
@@ -4,8 +4,13 @@
 #Brute force: Calculate all intersection points and take minimum M.D 
 #Mark each spot traveled (Indicate difference between line 1 and line 2)
 #If mark already exists from other line, add the coordinate of intersection to the list and continue 
+#Part 2: Return the minimum number of steps taken by both wires to reach an intersection (R8 = 8 steps)
+
 import sys
-import math 
+import math
+
+dy = {"L":0, "U":1, "R":0, "D":-1}
+dx = {"L":-1, "U":0, "R":1, "D":0}
 
 def stripOp(op):
     pair = []
@@ -13,70 +18,31 @@ def stripOp(op):
     pair.append(int(op[1:]))
     return pair
 
-#Assumes origin is at (0,0)
-def distance(p):
-    return abs(p[0]-0) + abs(p[1]-0)
-
 def findMin(points):
-    minimum = sys.maxsize
-    for each in points: 
-        temp = distance(each)
-        minimum = min(minimum,temp)
-    return minimum 
+    temp = min(points)
+    return(abs(temp[0])+abs(temp[1]))
 
-#Returns the last coordinate it left off on 
-def addLine(inst, p1, lSet1, lSet2, intersections):
-    i = 1 
-    op = inst[0] #L,R,U,D
-    amount = inst[1] #How far to travel 
-    if op == "U":
-        while i < amount:
-            if (p1[0], p1[1]+i) in lSet2: 
-                intersections.append((p1[0], p1[1]+i))
-            lSet1.add((p1[0], p1[1]+i))
-            i += 1 
-        p1[1] += i 
-    elif op == "D":
-        while i < amount:
-            if (p1[0], p1[1]-i) in lSet2: 
-                intersections.append((p1[0], p1[1]-i))
-            lSet1.add((p1[0], p1[1]-i))
-            i += 1 
-        p1[1] -= i 
-    elif op == "L":
-        while i < amount:
-            if (p1[0]-1, p1[1]) in lSet2: 
-                intersections.append((p1[0]-1, p1[1]))
-            lSet1.add((p1[0]-i, p1[1]))
-            i += 1 
-        p1[0] -= i 
-    else: 
-        assert op == "R" 
-        while i < amount:
-            if (p1[0]+1, p1[1]) in lSet2: 
-                intersections.append((p1[0]+1, p1[1]))
-            lSet1.add((p1[0]+i, p1[1]))
-            i += 1 
-        p1[0] += i 
-    return(p1)
+def points(arr):
+    x = 0 
+    y = 0 
+    seen = set()
+    for each in arr: 
+        temp = stripOp(each)
+        d = temp[0]
+        n = temp[1]
+        for i in range(n):
+            x += dx[d]
+            y += dy[d]
+            seen.add((x,y))
+    return seen
+
 
 a = [x for x in open("input.txt").read().split("\n")]
 l1 = a[0].split(",")
 l2 = a[1].split(",")
-l1Seen = set() 
-l2Seen = set() 
-curr = [0,0]
-points = []
-
-for each in l1: 
-    op = stripOp(each)
-    curr = addLine(op, curr, l1Seen, l2Seen, points)
-curr = [0,0]
-for each in l2:
-    op = stripOp(each)
-    curr = addLine(op, curr, l2Seen, l1Seen, points) 
-print(findMin(points))
-
+intersections = points(l1) & points(l2)
+closest = min(intersections)
+print(abs(closest[0]) + abs(closest[1]))
 
 
     
