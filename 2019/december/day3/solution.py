0eb19e4e92d3178774cf971b8c7e38c94a5a8785
Nevan Samadhana <nsamadhana@gmail.com>
2019-12-23 14:07:07 -0800

    Day 3

--- 2019/december/day3/solution.py
+++ 2019/december/day3/solution.py
@@ -5,9 +5,11 @@
 #Mark each spot traveled (Indicate difference between line 1 and line 2)
 #If mark already exists from other line, add the coordinate of intersection to the list and continue 
 #Part 2: Return the minimum number of steps taken by both wires to reach an intersection (R8 = 8 steps)
+#Each step coordinate should maintain the number of steps that it took to get there 
+#
 
-import sys
 import math
+import sys
 
 dy = {"L":0, "U":1, "R":0, "D":-1}
 dx = {"L":-1, "U":0, "R":1, "D":0}
@@ -25,6 +27,8 @@ def findMin(points):
 def points(arr):
     x = 0 
     y = 0 
+    nSteps = 0
+    steps = {} 
     seen = set()
     for each in arr: 
         temp = stripOp(each)
@@ -33,16 +37,32 @@ def points(arr):
         for i in range(n):
             x += dx[d]
             y += dy[d]
+            nSteps += 1
+            steps.update({(x,y) : nSteps})
             seen.add((x,y))
-    return seen
+    return (seen, steps)
 
 
 a = [x for x in open("input.txt").read().split("\n")]
 l1 = a[0].split(",")
 l2 = a[1].split(",")
-intersections = points(l1) & points(l2)
-closest = min(intersections)
-print(abs(closest[0]) + abs(closest[1]))
+
+p1 = points(l1)
+p2 = points(l2)
+intersections = p1[0] & p2[0]
+#Take the minimum sum of steps for all intersections 
+minSteps = sys.maxsize
+for i in intersections: 
+    steps = p1[1][i] + p2[1][i]
+    minSteps = min(steps, minSteps)
+print(minSteps)
+    
+
+
+
+
+
+
 
 
     
